b07d2f50c5b034f7b3826c7293aed1f1efae644d
tungulin <tungulin2002@gmail.com>
2026-09-19 17:40:48 +0300

    main 🐹 added lefthook

--- .lefthook/commit-msg/branch-prefix.sh
+++ .lefthook/commit-msg/branch-prefix.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+
 emoji="🐹"
-branch=$(git symbolic-ref --short HEAD)
+branch=$(git symbolic-ref --short HEAD 2>/dev/null) || exit 0
 message=$(cat "$1")
 
 if echo "$message" | (! grep -q "^${branch} ${emoji}*") &&\
    echo "$message" | (! grep -q "^Merge branch*") &&\
    echo "$message" | (! grep -q "^Merge remote-tracking branch*");\
 then
-   echo "$branch $emoji $(cat $1)" > "$1"
+   echo "$branch $emoji $message" > "$1"
 fi
